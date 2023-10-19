82583cfa25c63beb226094f5a242f12d23860b2d
Ali Hamza <contactalihamza@gmail.com>
2023-10-19 21:57:22 +0500

    video thumbnail

--- lib/application/providers/file_manager_provider.dart
+++ lib/application/providers/file_manager_provider.dart
@@ -1,8 +1,10 @@
 import 'dart:developer';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
 
 class FileManagerProvider extends ChangeNotifier {
   bool _isAccessAllowed = false;
@@ -15,8 +17,18 @@ class FileManagerProvider extends ChangeNotifier {
   File get file => File(
       '/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/7e3483e083cc4f278230e6341aac3292.jpg');
 
-  // File get file2 => File(
-  //     '/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/93695ac9487f4052b548657144891155.mp4');
+  Future<Uint8List?> get file2 async {
+    final videofile = File(
+        '/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/93695ac9487f4052b548657144891155.mp4');
+    final uint8list = await VideoThumbnail.thumbnailData(
+      video: videofile.path,
+      imageFormat: ImageFormat.JPEG,
+      maxWidth:
+          128, // specify the width of the thumbnail, let the height auto-scaled to keep the source aspect ratio
+      quality: 25,
+    );
+    return uint8list;
+  }
 
   // Check if permission is granted
   Future<bool> checkPermission() async {
--- lib/presentation/home_page/ui/status_saver_images.dart
+++ lib/presentation/home_page/ui/status_saver_images.dart
@@ -9,11 +9,21 @@ class StatusSaverImages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final image = FileManagerProvider().file;
-    // final video = FileManagerProvider().file2;
+    final video = FileManagerProvider().file2;
     return Center(
         child: Column(
       children: [
         Image.file(image),
+        FutureBuilder(
+          future: video,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Image.memory(snapshot.data!);
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
       ],
     ));
   }
