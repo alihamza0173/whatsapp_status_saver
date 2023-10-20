301c7e984221d33a82738942cfa3f221f7dec1dc
Ali Hamza <contactalihamza@gmail.com>
2023-10-20 10:11:20 +0500

    design update

--- lib/presentation/home_page/ui/status_saver_images.dart
+++ lib/presentation/home_page/ui/status_saver_images.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:whatsapp_status_saver/application/providers/file_manager_provider.dart';
 
 class StatusSaverImages extends StatelessWidget {
@@ -15,28 +16,35 @@ class StatusSaverImages extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             final data = snapshot.data as List<FileSystemEntity>;
-            return GridView.builder(
-              itemCount: data.length,
-              padding: const EdgeInsets.all(8.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 8.0,
-                crossAxisSpacing: 8.0,
-              ),
-              itemBuilder: (context, index) {
-                final file = data[index];
-                return DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8.0),
-                    image: DecorationImage(
-                      image: FileImage(file as File),
-                      fit: BoxFit.scaleDown,
-                    ),
-                  ),
-                );
-              },
+            return StaggeredGrid.count(
+              crossAxisCount: 2,
+              axisDirection: AxisDirection.down,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+              children: data.map((e) => Image.file(e as File)).toList(),
             );
+            // return GridView.builder(
+            //   itemCount: data.length,
+            //   padding: const EdgeInsets.all(8.0),
+            //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //     crossAxisCount: 2,
+            //     mainAxisSpacing: 8.0,
+            //     crossAxisSpacing: 8.0,
+            //   ),
+            //   itemBuilder: (context, index) {
+            //     final file = data[index];
+            //     return DecoratedBox(
+            //       decoration: BoxDecoration(
+            //         color: Colors.white,
+            //         borderRadius: BorderRadius.circular(8.0),
+            //         image: DecorationImage(
+            //           image: FileImage(file as File),
+            //           fit: BoxFit.scaleDown,
+            //         ),
+            //       ),
+            //     );
+            //   },
+            // );
           } else {
             return const Center(child: CircularProgressIndicator());
           }
