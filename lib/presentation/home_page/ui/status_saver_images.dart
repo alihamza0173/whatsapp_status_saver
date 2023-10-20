721a5f2fc67e45fbc9d6e64882afcf8bf3b9993c
Ali Hamza <contactalihamza@gmail.com>
2023-10-20 08:18:56 +0500

    Status Saver Images

--- lib/presentation/home_page/ui/status_saver_images.dart
+++ lib/presentation/home_page/ui/status_saver_images.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:whatsapp_status_saver/application/providers/file_manager_provider.dart';
 
 class StatusSaverImages extends StatelessWidget {
   const StatusSaverImages({
@@ -7,13 +10,26 @@ class StatusSaverImages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Column(
-      children: [
-        Text('Status Saver Images'),
-        SizedBox(height: 8.0),
-        Text('Coming Soon'),
-      ],
-    ));
+    return StreamBuilder(
+        stream: fileManagerProvider.getFilesImages(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data as List<FileSystemEntity>;
+            return GridView.builder(
+              itemCount: data.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+              ),
+              itemBuilder: (context, index) {
+                final file = data[index];
+                return Image.file(file as File, fit: BoxFit.cover);
+              },
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        });
   }
 }
