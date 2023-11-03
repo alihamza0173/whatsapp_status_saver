d785160acb1bd4b9762ccee473585cc733e6b1f7
Ali Hamza <contactalihamza@gmail.com>
2023-11-03 15:47:10 +0500

    language settings

---
+++ lib/presentation/home_page/ui/settings/language_settings.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:whatsapp_status_saver/application/providers/settings_provider.dart';
+import 'package:whatsapp_status_saver/l10n/l10n.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class LanguageSettings extends StatelessWidget {
+  const LanguageSettings({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        _switchLanguage(context);
+      },
+      icon: const Icon(Icons.language),
+    );
+  }
+
+  Future<dynamic> _switchLanguage(BuildContext context) {
+    final provider = context.read<SettingsProvider>();
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(AppLocalizations.of(context)!.language),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: L10n.languages.entries
+                .map(
+                  (e) => RadioListTile(
+                    title: Text(e.value),
+                    value: e.key,
+                    groupValue: provider.locale?.languageCode,
+                    onChanged: (value) {
+                      provider.setLocale(Locale(e.key));
+                      Navigator.pop(context);
+                    },
+                  ),
+                )
+                .toList(),
+          ),
+        );
+      },
+    );
+  }
+}
