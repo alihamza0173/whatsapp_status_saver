79689a2ecce41ab662fa97eb0daa5f648937ba24
Ali Hamza <contactalihamza@gmail.com>
2023-11-07 10:06:11 +0500

    shared preferences to store settings

--- lib/application/providers/settings_provider.dart
+++ lib/application/providers/settings_provider.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider extends ChangeNotifier {
+  // Singleton
+  static final SettingsProvider _instance = SettingsProvider._internal();
+
+  factory SettingsProvider() {
+    return _instance;
+  }
+
+  SettingsProvider._internal();
+
+  // Initial settings
+  Future<void> init() async {
+    final prefs = await SharedPreferences.getInstance();
+    final languageCode = prefs.getString('languageCode') ?? 'en';
+    _locale = Locale(languageCode);
+    final themeModeIndex = prefs.getInt('themeMode') ?? 0;
+    _themeMode = ThemeMode.values[themeModeIndex];
+  }
+
   // Language settings
   Locale? _locale;
   Locale? get locale => _locale;
   void setLocale(Locale locale) {
     _locale = locale;
+    final prefs = SharedPreferences.getInstance();
+    prefs.then((value) => value.setString('languageCode', locale.languageCode));
     notifyListeners();
   }
 
@@ -23,6 +44,10 @@ class SettingsProvider extends ChangeNotifier {
 
   void toggleTheme(bool isDarkMode) {
     _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    final prefs = SharedPreferences.getInstance();
+    prefs.then((value) => value.setInt('themeMode', _themeMode.index));
     notifyListeners();
   }
 }
+
+final SettingsProvider settingsProvider = SettingsProvider();
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_status_saver/application/application.dart';
 import 'package:whatsapp_status_saver/application/injector.dart';
+import 'package:whatsapp_status_saver/application/providers/settings_provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SettingsProvider().init();
   runApp(const Injector(child: MyApp()));
 }
