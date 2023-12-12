dd968c171e4f97654feac25da90072f9204dccad
Ali Hamza <contactalihamza@gmail.com>
2023-12-12 21:05:19 +0500

    use clear text traffic android

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:whatsapp_status_saver/application/injector.dart';
 import 'package:whatsapp_status_saver/application/providers/settings_provider.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await SettingsProvider().initPrefs();
   runApp(const Injector(child: MyApp()));
